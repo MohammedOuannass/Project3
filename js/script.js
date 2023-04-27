e3bb0a3bd46287b1e8911ee8edbf71aa164aa4d8
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-04-26 22:58:33 -0500

    cleaning up the java script

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map-container'), {
-    center: {lat: 41.8789, lng: -87.6359}, // Willis Tower coordinates
-    zoom: 14
-  });
+var map;
 
+function initMap() {
+  var willisTower = {lat: 41.878876, lng: -87.635915};
+  map = new google.maps.Map(document.getElementById('map-container'), {
+    zoom: 15,
+    center: willisTower
+  });
+  var markerIcon = {
+    url: 'media/towermarker.png',
+    size: new google.maps.Size(64, 64),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(16, 64),
+    scaledSize: new google.maps.Size(32, 32)
+  };
   var marker = new google.maps.Marker({
-    position: {lat: 41.8789, lng: -87.6359}, // Willis Tower coordinates
+    position: willisTower,
     map: map,
+    icon: markerIcon,
     title: 'Willis Tower'
   });
 }
-
-// Call the initMap function when the page loads
-google.maps.event.addDomListener(window, 'load', initMap);
-
-
-// Custom Marker
-var markerLocation = {lat: 41.8789, lng: -87.6359};
-
-var markerIcon = {
-  url: 'media/towermarker.png',
-  size: new google.maps.Size(64, 64),
-  origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(16, 64),
-  scaledSize: new google.maps.Size(32, 32)
-};
-
-var marker = new google.maps.Marker({
-  position: markerLocation,
-  map: map,
-  icon: markerIcon,
-  title: 'Willis Tower'
-});
