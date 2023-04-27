e6977e8687f301eba1f735ff7016d96f272a8a6f
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-04-26 22:50:17 -0500

    adding map function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map-container'), {
+    center: {lat: 41.8789, lng: -87.6359}, // Willis Tower coordinates
+    zoom: 14
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 41.8789, lng: -87.6359}, // Willis Tower coordinates
+    map: map,
+    title: 'Willis Tower'
+  });
+}
+
+// Call the initMap function when the page loads
+google.maps.event.addDomListener(window, 'load', initMap);
+
+
 // Custom Marker
 var markerLocation = {lat: 41.8789, lng: -87.6359};
 
