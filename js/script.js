c966605704482e114c7f4097a1499bf3332ce417
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-04-26 22:47:59 -0500

    fixing script

--- js/script.js
+++ js/script.js
@@ -1,55 +1,17 @@
-//Custom map marker
-var mapIframe = document.querySelector("#map-container iframe");
+// Custom Marker
+var markerLocation = {lat: 41.8789, lng: -87.6359};
 
-mapIframe.addEventListener("load", function() {
-  var mapDoc = mapIframe.contentDocument;
-  var marker = new mapDoc.defaultView.google.maps.Marker({
-    position: { lat: 41.878876, lng: -87.638490 },
-    map: mapDoc.defaultView.google.maps.Map,
-    icon: {
-      url: 'media/towermarker.png',
-      scaledSize: new mapDoc.defaultView.google.maps.Size(50, 50)
-    }
-  });
-});
+var markerIcon = {
+  url: 'media/towermarker.png',
+  size: new google.maps.Size(64, 64),
+  origin: new google.maps.Point(0, 0),
+  anchor: new google.maps.Point(16, 64),
+  scaledSize: new google.maps.Size(32, 32)
+};
 
-//Street View
-var mapIframe = document.querySelector("#map-container iframe");
-
-mapIframe.addEventListener("load", function() {
-  var mapDoc = mapIframe.contentDocument;
-  var panorama = new mapDoc.defaultView.google.maps.StreetViewPanorama(
-    mapDoc.getElementById('map-container'),
-    {
-      position: { lat: 41.878876, lng: -87.638490 },
-      pov: { heading: 165, pitch: 0 },
-      zoom: 1,
-      panControl: true,
-      zoomControl: true
-    }
-  );
-  mapDoc.defaultView.google.maps.Map.setStreetView(panorama);
-});
-
-//Direction Services
-var mapIframe = document.querySelector("#map-container iframe");
-
-mapIframe.addEventListener("load", function() {
-  var mapDoc = mapIframe.contentDocument;
-  var directionsService = new mapDoc.defaultView.google.maps.DirectionsService();
-  var directionsRenderer = new mapDoc.defaultView.google.maps.DirectionsRenderer();
-  directionsRenderer.setMap(mapDoc.defaultView.google.maps.Map);
-  directionsService.route({
-    origin: 'Chicago, IL',
-    destination: 'Los Angeles, CA',
-    travelMode: 'DRIVING',
-    unitSystem: mapDoc.defaultView.google.maps.UnitSystem.IMPERIAL,
-    avoidTolls: true
-  }, function(response, status) {
-    if (status === 'OK') {
-      directionsRenderer.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
+var marker = new google.maps.Marker({
+  position: markerLocation,
+  map: map,
+  icon: markerIcon,
+  title: 'Willis Tower'
 });
