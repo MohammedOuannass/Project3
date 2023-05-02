595acc9945ebc7cab9db7dd9af92b8f33067d1e0
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-05-02 18:40:52 -0500

    indentation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 function initMap() {
-  var location = {lat: 41.8789, lng: -87.6359};
+  var location = {
+    lat: 41.8789,
+    lng: -87.6359
+  };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: location
   });
-  
+
   var marker = new google.maps.Marker({
     position: location,
     map: map,
   });
-  
+
   var contentString = '<div id="content">' +
     '<h1 id="firstHeading" class="firstHeading">Sears Tower</h1>' +
     '<div id="bodyContent">' +
@@ -17,7 +20,7 @@ function initMap() {
     '<p>The Sears Tower is a 110-story, 1,450-foot skyscraper in Chicago, Illinois.</p>' +
     '</div>' +
     '</div>';
-  
+
   var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
