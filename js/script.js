3c7eaf6175a89f4711208906a1c95c6babe66ac3
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-05-02 17:19:02 -0500

    working out java script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
-var map;
-
 function initMap() {
-  var willisTower = {lat: 41.878876, lng: -87.635915};
-  map = new google.maps.Map(document.getElementById('map-container'), {
-    zoom: 15,
-    center: willisTower
-  });
-  var markerIcon = {
-    url: 'media/towermarker.png',
-    size: new google.maps.Size(64, 64),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(16, 64),
-    scaledSize: new google.maps.Size(32, 32)
-  };
-  var marker = new google.maps.Marker({
-    position: willisTower,
-    map: map,
-    icon: markerIcon,
-    title: 'Willis Tower'
+  var location = {lat: 41.8789, lng: -87.6359};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: location
   });
 }
